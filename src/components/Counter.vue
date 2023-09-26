0b2d1eb2ba3301d50b8509845ea30be474f854f5
Daniel Kelly <me@danielkelly.io>
2023-09-26 14:47:25 -0500

    vue

---
+++ src/components/Counter.vue
@@ -0,0 +1,10 @@
+<script setup lang="ts">
+import { ref } from "vue";
+const props = defineProps<{
+  startingCount?: number;
+}>();
+const counter = ref(props.startingCount || 0);
+</script>
+<template>
+  <button @click="counter++"><slot></slot>{{ counter }}</button>
+</template>
